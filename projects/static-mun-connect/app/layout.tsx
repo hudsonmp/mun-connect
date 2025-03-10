7b24db028c8ff2ccfc50681ad2ebe660e9ed428c
hudsonmp <hudsonmitchellpullman@gmail.com>
2025-03-09 21:13:29 -0700

    idek

--- projects/static-mun-connect/app/layout.tsx
+++ projects/static-mun-connect/app/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "../components/ui/toaster";
+import { Toaster } from "components/ui/toaster";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
